c4b0af20bbd0085922738654d144906481855467
YENA <dr_noah@naver.com>
2025-01-03 16:11:01 +0900

    back commit 5

--- back/controller/Ctodo.js
+++ back/controller/Ctodo.js
@@ -1,21 +1,34 @@
 const { Todo } = require("../models/index");
+console.log(Todo);
 
 /* Todos 전체 목록 불러오기 */
 exports.readAll = async (req, res) => {
-  console.log(Todo());
-  res.render(todo);
+  res.render("todo");
+  console.log(readAll);
 };
 
 /* Todo 한 개 불러오기 */
 exports.readOne = async (req, res) => {
   console.log(readOne);
+  Visitor.sequelize(req.params.id, (result) => {
+    console.log("Todo", result);
+    res.send(result);
+  });
 };
 
 /* 새로운 Todo 생성 */
 exports.create = async (req, res) => {};
 
 /* 기존 Todo 수정 */
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+  console.log(req.body);
+  Visitor.sequelize(req.body, () => {
+    res.send("수정");
+  });
+};
 
 /* 기존 Todo 삭제 */
-exports.delete = async (req, res) => {};
+exports.delete = async (req, res) => {
+  if (err) throw err;
+  console.log("데이터 삭제", rows);
+};
